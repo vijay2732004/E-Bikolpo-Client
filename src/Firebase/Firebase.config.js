c2fcc68387f6af3ad73ca8a876f454308ec79a93
shakib <shakiblp333@gmail>
2025-06-11 12:01:57 +0600

    firebase modified

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBMRLpgoCq-1tlarpLq0MB99Gma0gB1N88",
-  authDomain: "e-bikolpo.firebaseapp.com",
-  projectId: "e-bikolpo",
-  storageBucket: "e-bikolpo.firebasestorage.app",
-  messagingSenderId: "527704471066",
-  appId: "1:527704471066:web:60abf1875aa34d91fd03a3",
-  measurementId: "G-1WK7XGVFK6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
